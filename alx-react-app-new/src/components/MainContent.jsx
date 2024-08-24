980012b3dfaa0433733c0edb9e21628b6b10a67e
sarahsameh232 <sarahsameh232@gmail.com>
2024-08-24 15:19:28 +0300

    reactApp-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <main style={{ color: "orange", textAlign: "center" }}>
+      <p style={{ fontSize: "32px" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+
