2f735bf04108e6091f246ddc5193b5a869ac3af0
sarahsameh232 <sarahsameh232@gmail.com>
2024-08-31 12:36:58 +0300

    added my-company

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,17 +1,15 @@
-import React, { useContext } from 'react';
-import UserContext from './components/UserContext';
+import React, { useContext } from "react";
+import UserContext from "./components/UserContext";
 
 function UserProfile() {
   const userData = useContext(UserContext);
 
-  return ( <div>
-    <h2>{props.name}</h2>
-    <p>Age: {props.age}</p>
-    <p>Bio: {props.bio}</p>
-</div>
-);
-
-
-
-
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
 export default UserProfile;
---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "10px",
+        backgroundColor: "#f4f4f4",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2024 My Company. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
