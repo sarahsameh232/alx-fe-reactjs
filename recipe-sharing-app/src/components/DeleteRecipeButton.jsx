ae56c3839875e8ff0d45f5819f509fb65f21d80a
sarahsameh232 <sarahsameh232@gmail.com>
2024-09-01 16:33:17 +0300

    button added

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,17 @@
-
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from "../components/recipeStore";
+
 const DeleteRecipeButton = ({ recipeId }) => {
     const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+    const navigate = useNavigate();
+  
+    const handleDelete = () => {
+      deleteRecipe(recipeId);
+      navigate('/'); // Navigate to the recipe list page after deletion
+    };
   
     return (
-      <button onClick={() => deleteRecipe(recipeId)}>
+      <button onClick={handleDelete}>
         Delete Recipe
       </button>
     );
